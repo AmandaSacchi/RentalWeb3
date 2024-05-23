25ae53c05e5ff16419d1494b54be3cdb48484f24
AmandaSacchi <amandasacchi@hotmail.com>
2024-05-22 21:07:28 -0300

    Frontend

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -11,15 +11,17 @@ contract RealEstate is ERC721URIStorage {
 
     constructor() ERC721("Real Estate", "REAL") {}
 
-    function mint( string memory tokenURI) public returns (uint256){
+    function mint(string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        
-        return newItemId;
 
+        return newItemId;
     }
 
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
 }
